94e1968059c1612a2cfa814479f2b0320f64d998
kimseungyoon <neptuner24@loplat.com>
2023-01-12 18:12:15 +0900

    초기 세팅

---
+++ src/main/java/com/example/onair_airbnb_mvn/OnairAirbnbMvnApplication.java
@@ -0,0 +1,13 @@
+package com.example.onair_airbnb_mvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnairAirbnbMvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnairAirbnbMvnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onair_airbnb_mvn/OnairAirbnbMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onair_airbnb_mvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnairAirbnbMvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
